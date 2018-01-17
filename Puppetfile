2ae2f06189bfad043a5465078bbb2814056a5160
Karl <karlgharios@hotmail.com>
2018-01-17 07:13:14 +0000

    17/01/2018 07:13:14

--- Puppetfile
+++ Puppetfile
@@ -9,3 +9,7 @@ mod 'puppetlabs/stdlib'
 
 mod 'puppetlabs/apache'
 
+mod 'maestrodev-wget', '1.7.3'
+
+mod 'puppet-rpmkey', git:'https://github.com/stschulte/puppet-rpmkey'
+
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,62 @@
-class {'apache':}
+# 3
+# apache::vhost {'mpli.example.com':
+# 	port => '8080',
+# 	docroot => '/tmp',
+# }
 
-apache::vhost {'mpli.example.com':
-	port => '8080',
-	docroot => '/tmp',
+
+package {'epel-release':
+	ensure => 'installed',
+	provider => 'yum',
+
 }
+
+
+package {'java-1.8.0-openjdk.x86_64':
+	ensure => 'installed',
+	provider => 'yum',
+
+}
+
+exec { 'yum_update':
+	command => 'sudo yum update -y',
+	path    => '/usr/local/bin/:/bin/',
+
+}
+
+include wget
+
+
+
+
+# include rpmkey
+
+rpmkey { 'D50582E6':
+  ensure => present,
+  source => 'https://pkg.jenkins.io/redhat-stable/jenkins.io.key',
+}
+
+
+
+
+wget::fetch { "download Jenkins Repo":
+  source      => 'https://pkg.jenkins.io/redhat-stable/jenkins.repo',
+  verbose     => false,
+  destination => '/etc/yum.repos.d/jenkins.repo'
+}
+
+
+package {'jenkins':
+	ensure => 'installed',
+	provider => 'yum',
+
+}
+
+# Package['java-1.8.0-openjdk.x86_64'] -> Exec['yum_update'] 
+
+
+# service {'jenkins':
+# 	name => 'jenkins.service',
+# 	ensure => 'running',
+# 	enable =>  'true',
+# }
